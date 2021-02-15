0c7a357b963c265a7cc284feb62b39bd02346bb5
BenWinchester <benedict.winchester@gmail.com>
2021-02-15 16:17:57 +0000

    145/02/2021 - 1619 - Glass MUT updated.

--- pvt_model/pvt_panel/tests/test_glass.py
+++ pvt_model/pvt_panel/tests/test_glass.py
@@ -14,8 +14,71 @@ of the panel.
 
 """
 
-import unittest  # pylint: disable=unused-import
+import unittest
 
 from unittest import mock  # pylint: disable=unused-import
 
-from .. import glass  # pylint: disable=unused-import
+import pytest
+
+from ...__utils__ import OpticalLayerParameters
+from .. import glass
+from .test_utils import PYTEST_PRECISION
+
+
+class _BaseTest(unittest.TestCase):
+    """
+    Contains base functionality to set up mocks and functions in common across tests.
+
+    """
+
+    def setUp(self) -> None:
+        """
+        Sets up mocks in common across test cases.
+
+        """
+
+        super().setUp()
+
+        self.glass_parameterse = OpticalLayerParameters(
+            mass=150,
+            heat_capacity=4000,
+            area=100,
+            thickness=0.015,
+            transmissivity=0.9,
+            absorptivity=0.88,
+            emissivity=0.5,
+        )
+        self.diffuse_reflection_coefficient = 0.18
+
+        self.glass = glass.Glass(
+            self.diffuse_reflection_coefficient, self.glass_parameterse
+        )
+
+
+class TestInstantiate(_BaseTest):
+    """
+    Tests the instantiation of the :class:`pv.PV` instance.
+
+    """
+
+    def test_instantiate(self) -> None:
+        """
+        Tests the instantisation of a :class:`pv.PV` instance.
+
+        This checks that all private attributes are set as expected.
+
+        """
+
+        self.assertEqual(
+            self.glass.diffuse_reflection_coefficient,
+            self.diffuse_reflection_coefficient,
+        )
+        self.assertEqual(self.glass.mass, self.glass_parameterse.mass)
+        self.assertEqual(self.glass.heat_capacity, self.glass_parameterse.heat_capacity)
+        self.assertEqual(self.glass.area, self.glass_parameterse.area)
+        self.assertEqual(self.glass.thickness, self.glass_parameterse.thickness)
+        self.assertEqual(
+            self.glass.transmissivity, self.glass_parameterse.transmissivity
+        )
+        self.assertEqual(self.glass.absorptivity, self.glass_parameterse.absorptivity)
+        self.assertEqual(self.glass.emissivity, self.glass_parameterse.emissivity)
