add65f52a4e04dc85a16d86367d08de45475d3bf
BenWinchester <benedict.winchester@gmail.com>
2021-02-20 13:36:06 +0000

    20/02/2021 - 1336 - Empty __main__ module created for the higher-component.

---
+++ pvt_model/__main__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3.7
+########################################################################################
+# __main__.py - The main module for the higher-level PV-T model.
+#
+# Author: Ben Winchester
+# Copyright: Ben Winchester, 2021
+########################################################################################
+
+"""
+The main module for the PV-T model.
+
+The model is run from here for several runs as determined by command-line arguments.
+
+"""
+
+import sys
+
+
+def main(args) -> None:
+    """
+    The main module for the code.
+
+    :param args:
+        The command-line arguments passed into the component.
+
+    """
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
