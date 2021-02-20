40e3af6031660a0f67f0614b983536cd6945a16e
BenWinchester <benedict.winchester@gmail.com>
2021-02-20 13:33:13 +0000

    20/02/2021 - 1333 - __init__.py modules added.

--- pvt_model/__init__.py
+++ pvt_model/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.7
 ########################################################################################
-# __init__.py - The init module for the PVT model component.
+# __init__.py - The init module for this PVT model. No functionality here.
 #
 # Author: Ben Winchester
-# Copyright: Ben Winchester, 2020
+# Copyright: Ben Winchester, 2021
 ########################################################################################
--- pvt_model/enforcement/__init__.py
+++ pvt_model/enforcement/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.7
 ########################################################################################
-# __init__.py - The init module for the PVT panel component. No functionality here.
+# __init__.py - The init module for the enforcement component.
 #
 # Author: Ben Winchester
 # Copyright: Ben Winchester, 2021
