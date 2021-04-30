e89c5f6714166608ad2f50aa46d823e7756010fd
BenWinchester <benedict.winchester@gmail.com>
2021-04-30 08:29:58 +0100

    Progress saved to qsubgs

--- scripts/qsubs.sh
+++ scripts/qsubs.sh
@@ -97,6 +97,9 @@ qsub scripts/hpc_scripts/ilaria_single_glazed_0_1_litres_per_hour_31_x_50
 qsub scripts/hpc_scripts/ilaria_single_glazed_0_2_litres_per_hour_31_x_50
 qsub scripts/hpc_scripts/ilaria_single_glazed_0_3_litres_per_hour_31_x_50
 qsub scripts/hpc_scripts/ilaria_single_glazed_0_4_litres_per_hour_31_x_50
+
+
+
 qsub scripts/hpc_scripts/ilaria_single_glazed_0_5_litres_per_hour_31_x_50
 qsub scripts/hpc_scripts/ilaria_single_glazed_0_6_litres_per_hour_31_x_50
 qsub scripts/hpc_scripts/ilaria_single_glazed_0_7_litres_per_hour_31_x_50
@@ -144,17 +147,16 @@ qsub scripts/hpc_scripts/i_g_t_0_90.sh
 qsub scripts/hpc_scripts/i_g_t_0_95.sh
 qsub scripts/hpc_scripts/i_high_therm_coeff.sh
 qsub scripts/hpc_scripts/i_pv_e_0.sh
-
-qsub scripts/hpc_scripts/a_0_05_elec_eff.sh
 qsub scripts/hpc_scripts/a_0_10_elec_eff.sh
+qsub scripts/hpc_scripts/a_0_05_elec_eff.sh
 qsub scripts/hpc_scripts/a_0_25_elec_eff.sh
 qsub scripts/hpc_scripts/a_g_e_0_0.sh
 qsub scripts/hpc_scripts/a_g_e_0_9.sh
-qsub scripts/hpc_scripts/a_g_t_0_85.sh
 qsub scripts/hpc_scripts/a_g_t_0_90.sh
-qsub scripts/hpc_scripts/a_g_t_0_95.sh
 qsub scripts/hpc_scripts/a_high_therm_coeff.sh
 qsub scripts/hpc_scripts/a_pv_e_0.sh
+qsub scripts/hpc_scripts/a_g_t_0_85.sh
+qsub scripts/hpc_scripts/a_g_t_0_95.sh
 qsub scripts/hpc_scripts/i_0_05_elec_eff.sh
 qsub scripts/hpc_scripts/i_0_10_elec_eff.sh
 qsub scripts/hpc_scripts/i_0_25_elec_eff.sh
