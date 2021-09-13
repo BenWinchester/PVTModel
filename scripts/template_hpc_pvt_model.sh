2c0682fcfab13e8221ba1f929393876552dfb8c4
BenWinchester <benedict.winchester@gmail.com>
2021-09-13 17:01:23 +0100

    Updated runtime on HPC end

--- scripts/template_hpc_pvt_model.sh
+++ scripts/template_hpc_pvt_model.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#PBS -lwalltime=12:00:00
+#PBS -lwalltime=72:00:00
 #PBS -lselect=1:ncpus=8:mem=11800Mb
 
 echo -e "HPC script executed"
 
 # Load the anaconda environment
-module load anaconda3/py37
+module load anaconda3/personal
 source activate py37
 
 OUTPUT_DIR="$PBS_O_WORKDIR/output_files"
